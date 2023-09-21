bb5f3f57cf2bc0d110d46fc910907a6d6f74743a
Jhon Achata <jhonachata.dev@gmail.com>
2023-09-20 22:24:57 -0500

    use slugs for tags and categories urls

---
+++ src/utils/slug.ts
@@ -0,0 +1,13 @@
+export const slugify = (text: string): string => {
+    return text
+        .toString()
+        .replace(/\s+/g, "-")
+        .replace(/^-+/, "")
+        .replace(/-+$/, "");
+}
+
+export const deslugify = (text: string): string => {
+    return text
+        .toString()
+        .replace(/-/g, " ");
+}
