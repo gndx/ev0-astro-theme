fc694d88108ce26a57e889d8529e2706fa04d313
McFlyPartages <44530252+McFlyPartages@users.noreply.github.com>
2023-12-02 22:03:16 +0100

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,6 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-  type: 'content',
   schema: z.object({
     title: z.string(),
     description: z.string(),
