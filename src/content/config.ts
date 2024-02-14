334634c3ddb35009dfd84a75fc99accd08bb48d4
McFlyPartages <github@mcflypartages.fr>
2024-02-14 18:44:34 +0100

    Fix image cover

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,13 +2,13 @@ import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
   type: 'content',
-  schema: z.object({
+  schema: ({ image }) => z.object({
     draft: z.boolean(),
     title: z.string(),
     description: z.string(),
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
+    heroImage: image().optional(),
     categories: z.array(z.string()).default(['others']),
     tags: z.array(z.string()).default(['others']),
     authors: z.array(z.string()).default(['mcfly']),
